8fa9118b4ca8e19a8ead7293a40c918e6c5362ce
Ksenia <k.k.molostvova@gmail.com>
2026-01-07 16:58:32 +0300

    Temporary save point

--- src/main/kotlin/Narrator.kt
+++ src/main/kotlin/Narrator.kt
@@ -1,15 +1,19 @@
 package org.example
 
+import kotlin.random.Random
+import kotlin.random.nextInt
+
+val narrationModifier={ message:String ->
+    val numExclamationPoints = 3
+    message.uppercase() + "!".repeat(numExclamationPoints)
+}
+
 fun narrate(
     message: String
 ){
-    val narrationModifier:()-> String = {
-        val numExclamationPoints = 3
-        message.uppercase() + "!".repeat(numExclamationPoints)
+   val narrationModifier: ()-> String={
+       val numExclamationPoints = 3
+       message.uppercase() + "!".repeat(numExclamationPoints)
     }
-    println({
-        val numExclamationPoints = 3
-        message.uppercase() + "!".repeat(numExclamationPoints)
-    }())
     println(narrationModifier())
 }
