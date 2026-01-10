2177c40a3c5a35aeaf73076ec0332bc74ac73337
Ksenia <k.k.molostvova@gmail.com>
2026-01-10 13:00:23 +0300

    added two more titles

--- src/main/kotlin/NyetHack.kt
+++ src/main/kotlin/NyetHack.kt
@@ -1,11 +1,9 @@
 package org.example
 
 fun main() {
-    narrate("A hero enters the town of Kronstadt. What is their name?")
-        {message->
-            //Выводит сообщение желтым цветом
-            "\u001b[33;1m$message\u001b[0m"
-        }
+    narrate("A hero enters the town of Kronstadt. What is their name?",
+        ::makeYellow)
+
     val heroName = readLine()
     require(heroName!=null&&heroName.isNotEmpty()){
         "Hero name cannot be empty"
@@ -15,8 +13,12 @@ fun main() {
     narrate("$heroName, ${createTitle(heroName)}, heads to the town square")
 }
 
+private fun makeYellow(message:String)="\u001b[33;1m$message\u001b[0m"
+
 private fun createTitle(name :String):String{
     return when{
+        name.all{it.isDigit()} -> "The Identifiable"
+        name.none{it.isLetter()} -> "The Witness Protection Member"
         name.count{it.lowercase()in "aeiou"}>4->"The Master of Vowel"
 else->"The Renowned Hero"
     }
