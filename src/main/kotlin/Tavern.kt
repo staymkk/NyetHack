e528c5191dcd7b982d092b1d3c55cafd99fc8a80
Ksenia <k.k.molostvova@gmail.com>
2026-01-10 14:13:40 +0300

    added the tavern visitors and their introduction

--- src/main/kotlin/Tavern.kt
+++ src/main/kotlin/Tavern.kt
@@ -4,5 +4,22 @@ private const val TAVERN_MASTER = "Tatooine"
 private const val TAVERN_NAME = "$TAVERN_MASTER's Folly"
 
 fun visitTavern(){
+narrate("$heroName enters $TAVERN_NAME")
 
+    val patrons = listOf("Eli","Mordoc","Sophie")
+    println(patrons[0])
+
+    val eliMessage = if (patrons.contains("Eli")){
+        "$TAVERN_MASTER says: Eli's in the back playing cards"
+    } else {
+        "$TAVERN_MASTER says: Eli isn't here"
+    }
+    println(eliMessage)
+
+    val othersMessage = if (patrons.containsAll(listOf("Sophie","Mordoc"))){
+"$TAVERN_MASTER says: Mordoc and Sophie are seated by the stew kettle"
+} else {
+    "$TAVERN_MASTER says: Sophie and Mordoc aren't with each other right now"
+    }
+    println(othersMessage)
 }
