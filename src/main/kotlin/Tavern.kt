00c072cb407ea051d065b39a37b2f32a959cae75
Ksenia <k.k.molostvova@gmail.com>
2026-01-11 13:12:56 +0300

    added name generation, updated the menu

--- src/main/kotlin/Tavern.kt
+++ src/main/kotlin/Tavern.kt
@@ -5,6 +5,46 @@ import java.io.File
 private const val TAVERN_MASTER = "Tatooine"
 private const val TAVERN_NAME = "$TAVERN_MASTER's Folly"
 
+private val firstNames = setOf(
+    "Elara",
+    "Kaelen",
+    "Brynn",
+    "Dorian",
+    "Lyra",
+    "Rurik",
+    "Isolde",
+    "Morgana",
+    "Gandalf",
+    "Merlin",
+    "Sypha",
+    "Conan",
+    "Xena",
+    "Ragnar",
+    "Brienne",
+    "Kratos",
+    "Garrett",
+    "Loki",
+    "Carmen",
+    "Robin"
+)
+private val lastNames = setOf(
+    "Spellweaver",
+    "Arcanum",
+    "Mystborne",
+    "Runecaster",
+    "Stargazer",
+    "Ironside",
+    "Battlehorn",
+    "Steelheart",
+    "Warhammer",
+    "Bloodaxe",
+    "Quickfinger",
+    "Shadowstep",
+    "Silentstalk",
+    "Nightshade",
+    "Cleverhand"
+)
+
 private val menuData = File("src/main/resources/tavern-menu-data.txt")
     .readText()
     .split("\n")
@@ -20,54 +60,65 @@ fun visitTavern() {
         ::makeGreen
     )
     narrate("There are several items for sale:")
-    println(menuItems)
+    narrate(
+        menuItems.joinToString(),
+        ::makeBlue
+    )
 
-    val patrons = mutableListOf("Eli", "Mordoc", "Sophie")
-    val readOnlyPatrons = patrons.toList()
-//    println(patrons[0])
-
-    val eliMessage = if (patrons.contains("Eli")) {
-        "$TAVERN_MASTER says: Eli's in the back playing cards"
-    } else {
-        "$TAVERN_MASTER says: Eli isn't here"
+    val patrons: MutableSet<String> = mutableSetOf()
+    repeat(10) {
+        patrons += "${firstNames.random()} ${lastNames.random()}"
     }
-    println(eliMessage)
 
-    val othersMessage = if (patrons.containsAll(listOf("Sophie", "Mordoc"))) {
-        "$TAVERN_MASTER says: Mordoc and Sophie are seated by the stew kettle"
-    } else {
-        "$TAVERN_MASTER says: Sophie and Mordoc aren't with each other right now"
-    }
-    println(othersMessage)
-    narrate("Eli leaves the tavern")
-    patrons.remove("Eli")
-    narrate("Alex enters the tavern")
-    patrons.add("Alex")
-    narrate("Alex (VIP) enters the tavern")
-    patrons.add(0, "Alex")
-    patrons[0] = "Alexis"
+//    val eliMessage = if (patrons.contains("Eli")) {
+//        "$TAVERN_MASTER says: Eli's in the back playing cards"
+//    } else {
+//        "$TAVERN_MASTER says: Eli isn't here"
+//    }
+//    println(eliMessage)
+//
+//    val othersMessage = if (patrons.containsAll(listOf("Sophie", "Mordoc"))) {
+//        "$TAVERN_MASTER says: Mordoc and Sophie are seated by the stew kettle"
+//    } else {
+//        "$TAVERN_MASTER says: Sophie and Mordoc aren't with each other right now"
+//    }
+//    println(othersMessage)
+
+//    narrate("Eli leaves the tavern")
+//    patrons.remove("Eli")
+//    narrate("Alex enters the tavern")
+//    patrons.add("Alex")
+//    narrate("Alex (VIP) enters the tavern")
+//    patrons.add(0, "Alex")
+//    patrons[0] = "Alexis"
 
 //    for (patron in patrons){
-//        println("Good evening, $patron")
+//        println("Good evening, $patron")desert dessert
 //    }
 
-    patrons.forEachIndexed { index, patron ->
-        println("Good evening, $patron - you're #${index + 1} in line")
-        placeOrder(patron, menuItems.random())
+//    patrons.forEachIndexed { index, patron ->
+//        println("Good evening, $patron - you're #${index + 1} in line")
+//        placeOrder(patron, menuItems.random())
+//    }
+
+    narrate("$heroName sees several patrons in the tavern:")
+    narrate(patrons.joinToString())
+
+    repeat(3) {
+        placeOrder(patrons.random(), menuItems.random())
     }
 }
-
-private fun placeOrder(patronName: String, menuItemName: String) {
-    narrate(
-        "$patronName speaks with $TAVERN_MASTER to place an order",
-        ::makeGreen
-    )
-    narrate(
-        "$TAVERN_MASTER hands $patronName a $menuItemName",
-        ::makeGreen
-    )
+    fun placeOrder(patronName: String, menuItemName: String) {
+        narrate(
+            "$patronName speaks with $TAVERN_MASTER to place an order",
+            ::makeGreen
+        )
+        narrate(
+            "$TAVERN_MASTER hands $patronName a $menuItemName",
+            ::makeGreen
+        )
 
 //    menuData.forEachIndexed { index, data ->
 //        println("$index: $data")
 //    }
-}
+    }
